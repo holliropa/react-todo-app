f5b42382e4675289204d3e1984b5239937200781
holliropa <46962575+holliropa@users.noreply.github.com>
2026-01-20 21:20:11 +0100

    Set up routing with `react-router-dom` and create Home page template

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,11 @@
+﻿import { Header } from "@/components/Header";
+import { Tasks } from "@/components/Tasks";
+
+export function Home() {
+  return (
+    <>
+      <Header />
+      <Tasks />
+    </>
+  );
+}
---
+++ src/pages/Home/index.ts
@@ -0,0 +1,1 @@
+﻿export { Home } from "./Home";
